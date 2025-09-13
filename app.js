a7f5660cf09f6fa1684ba8be9b4407b0d1f3bc99
mjjyv <minhvueye363@gmail.com>
2025-09-13 14:53:37 +0700

    #3 commit: thêm hàm hiển thị tuổi dựa trên năm sinh

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ function displayInfo(name, age) {
   console.log(`Tuổi: ${age}`);
 }
 
+function calculateAge(birthYear) {
+    const currentYear = 2025
+    return currentYear - birthYear
+}
+
+const birthYear = 2000
+const age = calculateAge(birthYear)
+
 displayInfo("Alice", 30);
+
+console.log(`Với năm sinh ${birthYear}, tuổi của bạn là ${age}`)
